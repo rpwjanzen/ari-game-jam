8f03c0eb2ba8091219f7084afc4c54b28b1aa95d
rpwjanzen@gmail.com <rpwjanzen@gmail.com@bb64ad5d-38b5-b739-d900-13f7adffc5dc>
2012-01-29 05:42:48 +0000

    Addede logs to map.

---
+++ trunk/Squeeze/Squeeze/Squeeze/Entities/Log.Generated.cs
@@ -0,0 +1,267 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Model;
+
+using FlatRedBall.Input;
+using FlatRedBall.Utilities;
+
+using FlatRedBall.Instructions;
+using FlatRedBall.Math.Splines;
+using BitmapFont = FlatRedBall.Graphics.BitmapFont;
+using Cursor = FlatRedBall.Gui.Cursor;
+using GuiManager = FlatRedBall.Gui.GuiManager;
+// Generated Usings
+using Squeeze.Screens;
+using Matrix = Microsoft.Xna.Framework.Matrix;
+using FlatRedBall;
+using FlatRedBall.Graphics;
+using FlatRedBall.Math;
+using Squeeze.Performance;
+using FlatRedBall.Broadcasting;
+using Squeeze.Entities;
+using Squeeze.Factories;
+using FlatRedBall;
+using FlatRedBall;
+
+#if XNA4
+using Color = Microsoft.Xna.Framework.Color;
+#else
+using Color = Microsoft.Xna.Framework.Graphics.Color;
+#endif
+
+#if FRB_XNA || SILVERLIGHT
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+#endif
+
+#if FRB_XNA && !MONODROID
+using Model = Microsoft.Xna.Framework.Graphics.Model;
+#endif
+
+namespace Squeeze.Entities
+{
+	public partial class Log : PositionedObject, IDestroyable, IPoolable
+	{
+        // This is made global so that static lazy-loaded content can access it.
+        public static string ContentManagerName
+        {
+            get;
+            set;
+        }
+
+		// Generated Fields
+		#if DEBUG
+		static bool HasBeenLoadedWithGlobalContentManager = false;
+		#endif
+		static object mLockObject = new object();
+		static bool mHasRegisteredUnload = false;
+		static bool IsStaticContentLoaded = false;
+		private static Scene LogScene;
+		
+		private Sprite EntireScene;
+		public int Index { get; set; }
+		public bool Used { get; set; }
+		protected Layer LayerProvidedByContainer = null;
+
+        public Log(string contentManagerName) :
+            this(contentManagerName, true)
+        {
+        }
+
+
+        public Log(string contentManagerName, bool addToManagers) :
+			base()
+		{
+			// Don't delete this:
+            ContentManagerName = contentManagerName;
+            InitializeEntity(addToManagers);
+
+		}
+
+		protected virtual void InitializeEntity(bool addToManagers)
+		{
+			// Generated Initialize
+			LoadStaticContent(ContentManagerName);
+			EntireScene = LogScene.Sprites.FindByName("log_big1").Clone();
+			
+			PostInitialize();
+			if (addToManagers)
+			{
+				AddToManagers(null);
+			}
+
+
+		}
+
+// Generated AddToManagers
+		public virtual void AddToManagers (Layer layerToAddTo)
+		{
+			LayerProvidedByContainer = layerToAddTo;
+			SpriteManager.AddPositionedObject(this);
+			AddToManagersBottomUp(layerToAddTo);
+			CustomInitialize();
+		}
+
+		public virtual void Activity()
+		{
+			// Generated Activity
+			
+			CustomActivity();
+			
+			// After Custom Activity
+		}
+
+		public virtual void Destroy()
+		{
+			// Generated Destroy
+			SpriteManager.RemovePositionedObject(this);
+			if (Used)
+			{
+				LogFactory.MakeUnused(this, false);
+			}
+			
+			if (EntireScene != null)
+			{
+				SpriteManager.RemoveSprite(EntireScene);
+			}
+
+
+			CustomDestroy();
+		}
+
+		// Generated Methods
+		public virtual void PostInitialize ()
+		{
+		}
+		public virtual void AddToManagersBottomUp (Layer layerToAddTo)
+		{
+			// We move this back to the origin and unrotate it so that anything attached to it can just use its absolute position
+			float oldRotationX = RotationX;
+			float oldRotationY = RotationY;
+			float oldRotationZ = RotationZ;
+			
+			float oldX = X;
+			float oldY = Y;
+			float oldZ = Z;
+			
+			X = 0;
+			Y = 0;
+			Z = 0;
+			RotationX = 0;
+			RotationY = 0;
+			RotationZ = 0;
+			SpriteManager.AddToLayer(EntireScene, layerToAddTo);
+			if (EntireScene.Parent == null)
+			{
+				EntireScene.AttachTo(this, true);
+			}
+			X = oldX;
+			Y = oldY;
+			Z = oldZ;
+			RotationX = oldRotationX;
+			RotationY = oldRotationY;
+			RotationZ = oldRotationZ;
+		}
+		public virtual void ConvertToManuallyUpdated ()
+		{
+			this.ForceUpdateDependenciesDeep();
+			SpriteManager.ConvertToManuallyUpdated(this);
+			SpriteManager.ConvertToManuallyUpdated(EntireScene);
+		}
+		public static void LoadStaticContent (string contentManagerName)
+		{
+			ContentManagerName = contentManagerName;
+			#if DEBUG
+			if (contentManagerName == FlatRedBallServices.GlobalContentManager)
+			{
+				HasBeenLoadedWithGlobalContentManager = true;
+			}
+			else if (HasBeenLoadedWithGlobalContentManager)
+			{
+				throw new Exception("This type has been loaded with a Global content manager, then loaded with a non-global.  This can lead to a lot of bugs");
+			}
+			#endif
+			if (IsStaticContentLoaded == false)
+			{
+				IsStaticContentLoaded = true;
+				lock (mLockObject)
+				{
+					if (!mHasRegisteredUnload && ContentManagerName != FlatRedBallServices.GlobalContentManager)
+					{
+						FlatRedBallServices.GetContentManagerByName(ContentManagerName).AddUnloadMethod("LogStaticUnload", UnloadStaticContent);
+						mHasRegisteredUnload = true;
+					}
+				}
+				bool registerUnload = false;
+				if (!FlatRedBallServices.IsLoaded<Scene>(@"content/entities/log/logscene.scnx", ContentManagerName))
+				{
+					registerUnload = true;
+				}
+				LogScene = FlatRedBallServices.Load<Scene>(@"content/entities/log/logscene.scnx", ContentManagerName);
+				if (registerUnload && ContentManagerName != FlatRedBallServices.GlobalContentManager)
+				{
+					lock (mLockObject)
+					{
+						if (!mHasRegisteredUnload && ContentManagerName != FlatRedBallServices.GlobalContentManager)
+						{
+							FlatRedBallServices.GetContentManagerByName(ContentManagerName).AddUnloadMethod("LogStaticUnload", UnloadStaticContent);
+							mHasRegisteredUnload = true;
+						}
+					}
+				}
+				CustomLoadStaticContent(contentManagerName);
+			}
+		}
+		public static void UnloadStaticContent ()
+		{
+			IsStaticContentLoaded = false;
+			mHasRegisteredUnload = false;
+			if (LogScene != null)
+			{
+				LogScene.RemoveFromManagers(ContentManagerName != "Global");
+				LogScene= null;
+			}
+		}
+		public static object GetStaticMember (string memberName)
+		{
+			switch(memberName)
+			{
+				case  "LogScene":
+					return LogScene;
+			}
+			return null;
+		}
+		object GetMember (string memberName)
+		{
+			switch(memberName)
+			{
+				case  "LogScene":
+					return LogScene;
+					break;
+			}
+			return null;
+		}
+		protected bool mIsPaused;
+		public override void Pause (InstructionList instructions)
+		{
+			base.Pause(instructions);
+			mIsPaused = true;
+		}
+		public virtual void SetToIgnorePausing ()
+		{
+			InstructionManager.IgnorePausingFor(this);
+			InstructionManager.IgnorePausingFor(EntireScene);
+		}
+
+    }
+	
+	
+	// Extra classes
+	public static class LogExtensionMethods
+	{
+	}
+	
+}
---
+++ trunk/Squeeze/Squeeze/Squeeze/Entities/Log.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Graphics.Animation;
+using FlatRedBall.Graphics.Particle;
+
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.Math.Splines;
+using BitmapFont = FlatRedBall.Graphics.BitmapFont;
+using Cursor = FlatRedBall.Gui.Cursor;
+using GuiManager = FlatRedBall.Gui.GuiManager;
+
+#if FRB_XNA || SILVERLIGHT
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+using FarseerPhysics.Dynamics;
+
+
+#endif
+
+namespace Squeeze.Entities
+{
+	public partial class Log
+	{
+        public Body Body { get; set; }
+
+		private void CustomInitialize()
+		{
+
+
+		}
+
+		private void CustomActivity()
+		{
+            this.Position.X = Body.Position.X;
+            this.Position.Y = Body.Position.Y;
+
+            this.RotationZ = Body.Rotation;
+
+		}
+
+		private void CustomDestroy()
+		{
+
+
+		}
+
+        private static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+	}
+}
